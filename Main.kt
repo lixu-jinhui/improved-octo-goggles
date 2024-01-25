92879de0fc1405ab8f42251a372318d8d842f28a
LiXu <lixu1012878903@gmail.com>
2024-01-25 16:32:43 +0800

    test

--- Main.kt
+++ Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     val a: Float = 12345678.56789f
-    println("Hello World!")
+    println("Hello World! hahah")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.

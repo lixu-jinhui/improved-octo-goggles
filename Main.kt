81f66abc6323a7fd51a5efdab1d72c8ea1e7cf02
LiXu <1012878903@qq.com>
2024-01-25 16:20:28 +0800

    init

---
+++ Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val a: Float = 12345678.56789f
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("a = $a")
+}
